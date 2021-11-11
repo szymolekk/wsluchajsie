898c506767f1ae40456dabd79fd685156118601d
Szymolekk <luxszymolek@gmail.com>
2021-11-11 22:40:39 +0100

    Zmiana Tekstu zaleznie od czasu

--- script.js
+++ script.js
@@ -54,22 +54,116 @@ if (audio) {
   });
 }
 
-window.onload = ( function () {
-  getDataTime();
-  timer();
-})();
 
 function getDataTime() {
   var now = new Date();
   var hour = now.getHours();
-  if(hour >= 8) {
+  console.log(hour);
+  if(hour == 0) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 1) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 2) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 3) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 4) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 5) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 6) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 7) {
+    document.getElementById("humor").innerHTML = "Dzień Dobry ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 8) {
+    document.getElementById("humor").innerHTML = "Dzień Dobry ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 9) {
+    document.getElementById("humor").innerHTML = "Dzień Dobry ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 10) {
     document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
     humor.style.left = "50%";
   }
-  else {
-    if(hour <= 18) {
-      document.getElementById("humor").innerHTML = "Miłego Wieczoru ♡"
-      humor.style.left = "50%";
-    }
+  if(hour == 11) {
+    document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 12) {
+    document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 13) {
+    document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 14) {
+    document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 15) {
+    document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 16) {
+    document.getElementById("humor").innerHTML = "Miłego Dnia ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 17) {
+    document.getElementById("humor").innerHTML = "Miłego Wieczoru ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 18) {
+    document.getElementById("humor").innerHTML = "Miłego Wieczoru ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 19) {
+    document.getElementById("humor").innerHTML = "Miłego Wieczoru ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 20) {
+    document.getElementById("humor").innerHTML = "Miłego Wieczoru ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 21) {
+    document.getElementById("humor").innerHTML = "Miłego Wieczoru ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 22) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 23) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
+  }
+  if(hour == 24) {
+    document.getElementById("humor").innerHTML = "Dobranoc ♡"
+    humor.style.left = "50%";
   }
 }
+
+function all() {
+  getDataTime();
+  init();
+}
+
+window.onload = all;
